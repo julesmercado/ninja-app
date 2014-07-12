90b368eb2c12daa7bc8e41076e8ec447b0109e7f
eebasadre20 <eebasadre20@gmail.com>
2014-07-13 00:19:02 +0800

    Testing functionalities in node server

---
+++ client/lib/myangular-function.js
@@ -0,0 +1,66 @@
+var appModule = angular.module('myApp', []);
+var host = '192.168.1.42:8080/ninja/time/in'
+
+/*** Factory ***/
+
+appModule.factory('loginFactory', function ($http) {
+	return {
+		getHeroModel : function () {
+			return $http.get('lib/heromodel.json').then( function (result) {
+				return result.data;
+			});
+		}
+	}
+
+});
+
+appModule.factory('timeInFactory' , function ($http) {
+	return {
+		timeInHero : function ( $heroTimeIn ) {
+			return $http({
+				url		: host,		
+				method  : 'POST',
+				data 	: $heroTimeIn
+			});
+		}
+	}
+});
+
+
+/*** Controller ***/
+
+appModule.controller('loginController', function ($scope, loginFactory) {
+
+	var state = null;
+
+	loginFactory.getHeroModel().then(function (heroModelList) {
+		
+		for(var heroKey in heroModelList){
+			if( "state" in heroModelList[heroKey] || heroModelList[heroKey] === "out"  ) {
+				heroModelList[heroKey].state = "in";
+			} else {
+				heroModelList[heroKey].state = "out";
+			}
+		}
+
+		$scope.heroModel = heroModelList;
+	
+	});
+
+	$scope.timeIn = function ( heroInfo ) {
+		var dateAndTime = Date.now();
+		
+		$params = $.param({
+			"id" 			: heroInfo.id,
+			"name"  		: heroInfo.name,
+			"position"  	: heroInfo.position,
+			"dateAndTime" 	: dateAndTime,
+			"state"			: heroInfo.state
+			
+		});
+
+		timeInFactory.timeInHero($params);
+	}
+
+});
+
