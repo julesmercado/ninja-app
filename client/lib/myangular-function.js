e7ed707b5ba44dd427138462a67b96d0fb1056cf
eebasadre20 <eebasadre20@gmail.com>
2014-07-16 20:26:36 +0800

    Testing Registration with node server.

--- client/lib/myangular-function.js
+++ client/lib/myangular-function.js
@@ -1,6 +1,8 @@
 var appModule = angular.module('myApp', []);
-var host = 'http://192.168.1.42:8080/ninja/time/in';
-var production_location = '/static/lib/heromodel.json';
+var hostHeroIn = 'http://{{host}}:{{port}}/ninja/time/in';
+var hostHeroOut = 'http://{{host}}:{{port}}/ninja/time/out';
+var hostRegistration = 'http://{{host}}:{{port}}/ninja/add';
+var productionLocation = '/static/lib/heromodel.json';
 
 
 /*** Factory ***/
@@ -16,18 +18,52 @@ appModule.factory('loginFactory', function ($http) {
 
 });
 
+/*appModule.factory('loginFactory', function ($http) {
+	return {
+		getHeroModel : function (callback) {
+			return $http.get('lib/heromodel.json')
+			.success(function (data) {
+				callback(null, data);
+			})
+			.error(function ( error ) {
+				callback(error);
+			});
+		}
+	}
+
+});*/
+
 appModule.factory('timeInFactory' , function ($http) {
 	return {
 		timeInHero : function ( $heroTimeIn , callback) {
 			return $http({
-				url		: host,		
+				url		: hostHeroIn,		
 				method  : 'POST',
 				data 	: $heroTimeIn
 			})
 			.success(function (data) {
 				callback(null, data);
 			})
-			.error(function (error) {
+			.error(function (errorMsg) {
+				callback(errorMsg);
+			});
+		}
+	}
+
+});
+
+appModule.factory('timeOutFactory' , function ($http) {
+	return {
+		timeOutHero : function ( $heroTimeOut , callback) {
+			return $http({
+				url		: hostHeroOut,		
+				method  : 'POST',
+				data 	: $heroTimeOut
+			})
+			.success(function (data) {
+				callback(null, data);
+			})
+			.error(function (errorMsg) {
 				callback(error);
 			});
 		}
@@ -39,14 +75,14 @@ appModule.factory('registerHeroFactory', function ($http) {
 	return {
 		registerYourHero : function ( $heroRegisterInformation , callback) {
 			return $http({
-				url 	:  host,
+				url 	: hostRegistration,
 				method 	: 'POST',
 				data    : $heroRegisterInformation
 			})
 			.success(function (data) {
 				callback(null, data);
 			})
-			.error(function (error) {
+			.error(function (errorMsg) {
 				callback(error);
 			});
 		}
@@ -56,17 +92,20 @@ appModule.factory('registerHeroFactory', function ($http) {
 
 /*** Controller ***/
 
-appModule.controller('loginController', function ($scope, loginFactory) {
+appModule.controller('loginController', function ($scope, loginFactory, timeInFactory, timeOutFactory) {
 
 	var state = null;
+	var dateAndTime = Date.now();
 
 	loginFactory.getHeroModel().then(function (heroModelList) {
 		
 		for(var heroKey in heroModelList){
 			if( "state" in heroModelList[heroKey] || heroModelList[heroKey] === "out"  ) {
 				heroModelList[heroKey].state = "in";
+				state = "in";
 			} else {
 				heroModelList[heroKey].state = "out";
+				state = "out";
 			}
 		}
 
@@ -74,26 +113,47 @@ appModule.controller('loginController', function ($scope, loginFactory) {
 	
 	});
 
-	$scope.timeIn = function ( heroInfo ) {
-		var dateAndTime = Date.now();
-		
-		$params = $.param({
-			"id" 			: heroInfo.id,
-			"name"  		: heroInfo.name,
-			"position"  	: heroInfo.position,
-			"dateAndTime" 	: dateAndTime,
-			"state"			: heroInfo.state
-			
-		});
 
-		timeInFactory.timeInHero($params,  function(error, Data) {
-			if(error) {
-				alert(error);
-			} else {
-				alert(data);
-			}
-		});
-	}
+		$scope.timeIn = function ( heroInfo ) {
+			
+			$params = $.param({
+				"id" 			: heroInfo.id,
+				"name"  		: heroInfo.name,
+				"position"  	: heroInfo.position,
+				"dateAndTime" 	: dateAndTime,
+				"state"			: heroInfo.state
+			
+			});
+
+			timeInFactory.timeInHero($params,  function(error, Data) {
+				if(error) {
+					alert(error);
+				} else {
+					alert(data);
+				}
+			});
+		}
+	
+		$scope.timeOut = function ( heroInfo ) {
+		
+			$params = $.param({
+				"id" 			: heroInfo.id,
+				"name"  		: heroInfo.name,
+				"position"  	: heroInfo.position,
+				"dateAndTime" 	: dateAndTime,
+				"state"			: heroInfo.state
+			
+			});
+
+			timeOutFactory.timeOutHero($params,  function(error, Data) {
+				if(error) {
+					alert(error);
+				} else {
+					alert(data);
+				}
+			});
+		}
+		
 
 });
 
@@ -101,6 +161,8 @@ appModule.controller('loginController', function ($scope, loginFactory) {
 appModule.controller('registrationController', function ( $scope , registerHeroFactory ) {
 	$scope.heroInfo = {};
 	
+
+
 	$scope.registerHero = function ( heroInfo ) {
 		
 		$params = $.param({
@@ -109,11 +171,13 @@ appModule.controller('registrationController', function ( $scope , registerHeroF
 			"position"	: heroInfo.position
 		});
 
-		registerHeroFactory.registerYourHero($params, function(error, data) {
-			if(error) {
-				alert(error);
+		registerHeroFactory.registerYourHero($params, function(errorMsg, data) {
+			if(errorMsg) {
+				alert(errorMsg.error);
+				
 			} else {
-				alert(data);
+				alert(data.status);
+				
 			}
 		});
 	}
